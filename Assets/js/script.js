532f555cd5627d271ebaab230b679bc7648dead6
Daniel Covington <DanielmCovington@gmail.com>
2023-03-11 16:13:48 -0600

    Updated JS with full functionality and added comments. Additionally updated readme with new image of working preview and details on the project.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,8 +1,70 @@
-// Assignment code here
-
+// Global Variables 
+var numbers = [1,2,3,4,5,6,7,8,9,]
+var lowerLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u","v","w","x","y","z"]; 
+var Upperletter = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U","V","W","X","Y","Z"]; 
+var Special = "!@#$%^&*()_±=[]{}|;':,./<>?`-~";
+var SpecialArray = Special.split(""); 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+function RandomIndex(arr = []) {
+  var Index = arr[Math.floor(Math.random() * arr.length)];
+  return Index;
+}
+// Prompts
+function passwordPrompts() {
+  var lengthPrompt = window.prompt("How long would you like this password to be?");
+  var hasNumbers = window.confirm("Would you like to include numbers?");
+  var hasLowerCase = window.confirm("Would you like to have lowercase Letters?");
+  var hasUpperCase = window.confirm("Would you like to have Uppercase Letters?");
+  var hasSpecial = window.confirm("Would you like to have Special Characters?");
+  var parsedLength = Number.parseInt(lengthPrompt);
+
+// Validations 
+if (Number.isNaN(parsedLength) || (parsedLength < 8 || parsedLength > 128)) {
+window.alert("Invalid Entry, password length must be a number greater than 8 and less than 128");
+return null
+}
+
+if (!hasNumbers && !hasLowerCase && !hasUpperCase && !hasSpecial) {
+ window.alert("Please answer yes to at least 1 selection to generate your password");
+  return null; 
+  }
+
+return {
+  lengthPrompt,
+  hasNumbers,
+  hasLowerCase,
+  hasUpperCase,
+  hasSpecial,
+}
+}
+
+function generatePassword(){ 
+  var result = [] 
+  var chars = []
+  var prompts = passwordPrompts() 
+  if (prompts.hasLowerCase) {
+    // Validations
+    chars = chars.concat(lowerLetters)
+  }
+  if (prompts.hasUpperCase) {
+    chars = chars.concat(Upperletter) 
+  }
+  if (prompts.hasNumbers) {
+    chars = chars.concat(numbers)
+  }
+  if (prompts.hasSpecial) {
+    chars = chars.concat(SpecialArray)
+  }
+  for (let i = 0; i < prompts.lengthPrompt; i++) {
+    const element = RandomIndex(chars);
+    result.push(element)
+    }
+    /* 4. display Password to the page */ 
+    return result.join("");
+    }
 
 // Write password to the #password input
 function writePassword() {
--- README.md
+++ README.md
@@ -4,18 +4,12 @@
 # Notes 
 What all was done for creating my Password Generator 
 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-* 
-
+* Created Global Variable arrays that contained numbers, lowercase letters, uppercase letters, and special characters
+* Created a random index function to select from the above arrays at random
+* Created a passwordprompt function that prompts the user to let the program know what variables to use.
+* Created validation to only allow inputs of numbers from 8-128
+* Created Validation that only a number can be used for password length
+* Created validation that at least 1 password criteria is met(upper,lower,number,special)
 
 
 ##  Links
@@ -28,5 +22,5 @@ What all was done for creating my Password Generator
 
 The following image demonstrates the web application's appearance and functionality:
 
-![Preview of Website(Desktop)](./Assets/Images/Preview.png)
+![Preview of Website(Desktop)](./Assets/Images/FinalPreview.png)
 
